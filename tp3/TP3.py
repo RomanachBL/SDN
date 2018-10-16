3e4a134a1d73e69dc65ad54a6d684f734f6af344
RomanachBL <carrey.romain@gmail.com>
2018-10-16 10:12:45 +0200

    commit tp3

--- tp3/TP3.py
+++ tp3/TP3.py
@@ -1,7 +1,19 @@
-from sklearn import * 
+from sklearn import *
 import numpy as np
+
 iris = datasets.load_iris()
 X = iris.data
 Y = iris.target
 
-print(iris.target_names[(nei.predict(X[1,:]))])
+from sklearn.neighbors import KNeighborsClassifier
+nbors = KNeighborsClassifier(n_neighbors=1)
+nbors.fit(X, Y)
+i=0
+while i<len(X):
+    res = nbors.predict(X[i,:].reshape(1, -1))
+    '''print (res)'''
+    i=i+1
+
+'''err = sum(res != iris.target)
+print("Nb erreurs:", err)
+print( "Pourcentage de prédiction juste:", (150-err)*100/150)   # % de réussite'''
